5b3c3e5824f26f956e5d9fc9089911860c1dde80
emin.dzanko <emin.dzanko@isatis.com>
2025-01-15 13:37:33 +0100

    Added download pdf option

--- client/src/components/SegmentedImage/SegmentedImage.jsx
+++ client/src/components/SegmentedImage/SegmentedImage.jsx
@@ -1,6 +1,11 @@
+import { generatePDF } from '../../util/pdfutil';
 import './SegmentedImage.css';
 
 const SegmentedImage = ({ file, diagnosis }) => {
+    const handleDownloadPDF = () => {
+        generatePDF({ diagnosis }, file);
+    };
+
     return (
         <div className="segmented-image">
             <img
@@ -9,6 +14,9 @@ const SegmentedImage = ({ file, diagnosis }) => {
                 className="result-image"
             />
             <p className="diagnosis">Diagnosis: {diagnosis}</p>
+            <button className="download-pdf-button" onClick={handleDownloadPDF}>
+                Download PDF
+            </button>
         </div>
     );
 };
